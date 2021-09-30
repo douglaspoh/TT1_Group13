24e822b2cd98a49a97495a116c2a5264c379e78e
douglaspoh <duckglas@hotmail.com>
2021-09-30 11:39:37 +0800

    added route and useauth context

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,20 +5,43 @@ import mainPage from './routes/Mainpage';
 import loginPage from './routes/loginPage';
 import cartPage from './routes/cartPage';
 
+export const authContext = createContext();
 
 function Routes() {
+  const auth = useProvideAuth()
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar/>
-        <Switch>
-          <Route component={mainPage}/>
-          <Route component={loginPage}/>
-          <Route component={cartPage}/>
-        </Switch>
-      </BrowserRouter>
+      <authContext.Provider value={auth}>
+        <BrowserRouter>
+          <NavBar/>
+          <Switch>
+            <Route component={mainPage}/>
+            <Route component={loginPage}/>
+            <PrivateCart component={cartPage}/>
+          </Switch>
+        </BrowserRouter>
+      </authContext.Provider>
     </div>
   );
 }
 
+function useProvideAuth() {
+  const [user,setUser] = useState(null);
+
+  const signin = (cb) => {
+    setUser('user');
+    cb();
+  }
+
+  const signout = (cb) => {
+    setUser(null);
+    cb();
+  }
+
+  return {user,signin,signout}
+}
+
+
 export default Routes;
+
--- frontend/src/routes/mainPage.js
+++ frontend/src/routes/mainPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 function mainPage() {
     return (
         <div>
-            
+            Display products
         </div>
     )
 }
