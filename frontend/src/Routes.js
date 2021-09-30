b7bd77581cc1b2c647b5399fea7e32b503e1f422
engkhsky <engkh.sky@gmail.com>
2021-09-30 16:05:56 +0800

    Update all the add/remove for cart

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -53,10 +53,10 @@ function useProvideAuth() {
 
 function useCartOperations() {
   const auth = useContext(authContext);
-  const {products} = data;
-  const [cartList,setCartList] = useState([]);
-  const [addStatus,setAddStatus] = useState(false);
-  const [status,setStatus] = useState("Pending");
+  const { products } = data;
+  const [cartList, setCartList] = useState([]);
+  const [addStatus, setAddStatus] = useState(false);
+  const [status, setStatus] = useState("Pending");
   const [totalPrice, setTotalPrice] = useState('');
 
   const firstAdd = (product) => {
@@ -77,6 +77,33 @@ function useCartOperations() {
       )
       console.log(cartList)
     }
+
+    fetch('http://localhost:3005/order', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        customer_id: auth.user.id,
+        status: status,
+        totalPrice: totalPrice,
+      },
+        cartList
+      )
+    })
+      .then(res => {
+        if (!res.ok) {
+          console.log('Error!');
+        }
+        return res.text();
+      })
+      .then(data => {
+        console.log('success!');
+      })
+      .catch(err => {
+        console.log(err)
+      });
+
   };
 
   const onRemove = (product) => {
@@ -89,12 +116,64 @@ function useCartOperations() {
         cartList.map((item) => item.id === product.id ? { ...item, qty: item.qty - 1 } : item)
       )
     }
+
+    fetch('http://localhost:3005/order', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        customer_id: auth.user.id,
+        status: status,
+        totalPrice: totalPrice,
+      },
+        cartList
+      )
+    })
+      .then(res => {
+        if (!res.ok) {
+          console.log('Error!');
+        }
+        return res.text();
+      })
+      .then(data => {
+        console.log('success!');
+      })
+      .catch(err => {
+        console.log(err)
+      });
   }
 
   const onDelete = (product) => {
     setCartList(
       cartList.filter((item) => item.id !== product.id)
     )
+
+    fetch('http://localhost:3005/order', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        customer_id: auth.user.id,
+        status: status,
+        totalPrice: totalPrice,
+      },
+        cartList
+      )
+    })
+      .then(res => {
+        if (!res.ok) {
+          console.log('Error!');
+        }
+        return res.text();
+      })
+      .then(data => {
+        console.log('success!');
+      })
+      .catch(err => {
+        console.log(err)
+      });
   }
 
   const onSubmit = () => {
@@ -106,31 +185,35 @@ function useCartOperations() {
     setStatus("Purchased");
 
     fetch('http://localhost:3005/order', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(
-              cartList
-            )
-        })
-        .then(res=>{
-            if(!res.ok){
-                console.log('Error!');
-            }
-            return res.text();
-        })
-        .then(data=>{
-          console.log('success!');
-          // reset cartList
-          setCartList([]);
-        })
-        .catch(err=>{
-            console.log(err)
-        });      
-  } 
-  
-  return {products, cartList, addStatus, firstAdd, onAdd, onRemove, onDelete, onSubmit};
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        customer_id: auth.user.id,
+        status: status,
+        totalPrice: totalPrice
+      },
+        cartList
+      )
+    })
+      .then(res => {
+        if (!res.ok) {
+          console.log('Error!');
+        }
+        return res.text();
+      })
+      .then(data => {
+        console.log('success!');
+        // reset cartList
+        setCartList([]);
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+  return { products, cartList, addStatus, firstAdd, onAdd, onRemove, onDelete, onSubmit };
 
 }
 
